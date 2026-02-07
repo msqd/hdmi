780ed9b28691c89c04028532cb720e49fa8dd8b1
Romain Dorgueil <romain@makersquad.fr>
2026-02-07 05:11:27 +0100

    chore: bump version to 0.2.3

--- src/hdmi/__init__.py
+++ src/hdmi/__init__.py
@@ -7,7 +7,7 @@ A lightweight dependency injection framework with:
 - Early error detection
 """
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 
 from hdmi.builders import ContainerBuilder
 from hdmi.containers import Container, ScopedContainer
